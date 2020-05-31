44ddf98e17b2794b55fb8e7752fa4b35ee174840
Michael Page <mobilepage@me.com>
2020-05-31 15:49:02 +0930

    Create and connect IBOutlets and IBActions
    Also add additional view controller to later display info page website

--- Week01/ColorPicker/ColorPicker/ViewController.swift
+++ Week01/ColorPicker/ColorPicker/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorNameLabel: UILabel!
+
+    @IBOutlet weak var firstSliderTitleLabel: UILabel!
+    @IBOutlet weak var firstSlider: UISlider!
+    @IBOutlet weak var firstSliderValueLabel: UILabel!
+
+    @IBOutlet weak var secondSliderTitleLabel: UILabel!
+    @IBOutlet weak var secondSlider: UISlider!
+    @IBOutlet weak var secondSliderValueLabel: UILabel!
+
+    @IBOutlet weak var thirdSliderTitleLabel: UILabel!
+    @IBOutlet weak var thirdSlider: UISlider!
+    @IBOutlet weak var thirdSliderValueLabel: UILabel!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func setColor() {
+        
+    }
+
+    @IBAction func resetValues() {
+        
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        
+    }
 
 }
 
