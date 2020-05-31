e47d7907102d71473b5f7b0725a1c1383abe4024
Michael Page <mobilepage@me.com>
2020-05-31 15:02:07 +0930

    Add new project with initial storyboard layout
    Storyboard contains required labels, sliders and buttons

---
+++ Week01/ColorPicker/ColorPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorPicker
+//
+//  Created by Michael Page on 31/5/20.
+//  Copyright © 2020 Michael Page. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
