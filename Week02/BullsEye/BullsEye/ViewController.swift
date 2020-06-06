40be878d000356dcfbd500c77e25796bb1c86c2f
Michael Page <mobilepage@me.com>
2020-06-06 17:25:15 +0930

    Add BullsEye MVC refactor
    Refactor BullsEye to adhere to model–view–controller (MVC) design pattern. Moved game logic into structs BullsEyeGame and Rounds.

--- Week02/BullsEye/BullsEye/ViewController.swift
+++ Week02/BullsEye/BullsEye/ViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  var game = BullsEyeGame()
   var currentValue = 0
-  var targetValue = 0
-  var score = 0
-  var round = 0
+  var targetValue: Int? {
+    return game.currentRound?.targetValue
+  }
+  var score: Int {
+    return game.score
+  }
+  var round: Int {
+    return game.roundNumber
+  }
   
   @IBOutlet weak var slider: UISlider!
   @IBOutlet weak var targetLabel: UILabel!
@@ -24,44 +31,26 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     let roundedValue = slider.value.rounded()
     currentValue = Int(roundedValue)
-    startNewGame()
+    game.startNewGame()
+    updateView()
   }
 
   @IBAction func showAlert() {
-    
-    let difference = abs(targetValue - currentValue)
-    var points = 100 - difference
-    
-    score += points
-    
-    let title: String
-    if difference == 0 {
-      title = "Perfect!"
-      points += 100
-    } else if difference < 5 {
-      title = "You almost had it!"
-      if difference == 1 {
-        points += 50
-      }
-    } else if difference < 10 {
-      title = "Pretty good!"
-    } else {
-      title = "Not even close..."
+    guard let (title, message) = game.evaluatePlayerSelection(currentValue) else {
+      return
     }
-    
-    let message = "You scored \(points) points"
     
     let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
     
     let action = UIAlertAction(title: "OK", style: .default, handler: {
       action in
-      self.startNewRound()
+      self.game.startNewRound()
+      self.updateView()
     })
     
     alert.addAction(action)
     
     present(alert, animated: true, completion: nil)
-    
   }
   
   @IBAction func sliderMoved(_ slider: UISlider) {
@@ -69,24 +58,17 @@ class ViewController: UIViewController {
     currentValue = Int(roundedValue)
   }
   
-  func startNewRound() {
-    round += 1
-    targetValue = Int.random(in: 1...100)
+  func updateView() {
     currentValue = 50
     slider.value = Float(currentValue)
-    updateLabels()
-  }
-  
-  func updateLabels() {
-    targetLabel.text = String(targetValue)
+    targetLabel.text = targetValue != nil ? String(targetValue!) : "???"
     scoreLabel.text = String(score)
     roundLabel.text = String(round)
   }
   
   @IBAction func startNewGame() {
-    score = 0
-    round = 0
-    startNewRound()
+    game.startNewGame()
+    updateView()
   }
   
 }
