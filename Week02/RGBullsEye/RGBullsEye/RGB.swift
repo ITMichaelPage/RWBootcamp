76e1d3ebad98037d8cd0ef5fba8d3acae29c821e
Michael Page <mobilepage@me.com>
2020-06-07 11:50:54 +0930

    Add RGB randomize method
    Can be used with RGB.random()

--- Week02/RGBullsEye/RGBullsEye/RGB.swift
+++ Week02/RGBullsEye/RGBullsEye/RGB.swift
@@ -37,6 +37,13 @@ struct RGB {
   var g = 127
   var b = 127
   
+  static func random() -> RGB {
+    let r = Int.random(in: 0...255)
+    let g = Int.random(in: 0...255)
+    let b = Int.random(in: 0...255)
+    return RGB(r: r, g: g, b: b)
+  }
+  
   func difference(target: RGB) -> Double {
     let rDiff = Double(r - target.r)
     let gDiff = Double(g - target.g)
