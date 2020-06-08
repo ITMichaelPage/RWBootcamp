edacc11a1b513cc505cfebd61ccb7585262aa8aa
Michael Page <mobilepage@me.com>
2020-06-08 13:56:13 +0930

    Add minimal logic to view controller
    Slider moves to a random position, user can guess value and receive feedback

--- Week02/RevBullsEye/RevBullsEye/ViewController.swift
+++ Week02/RevBullsEye/RevBullsEye/ViewController.swift
@@ -8,13 +8,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+  
+  var game = BullsEyeGame()
+  var currentValue = 0
+  var targetValue: Int? {
+    return game.currentRound?.targetValue
+  }
+  var score: Int {
+    return game.score
+  }
+  var round: Int {
+    return game.roundNumber
+  }
+  var quickDiff: Int? {
+    guard let targetValue = game.currentRound?.targetValue else {
+      return nil
+    }
+    return abs(targetValue - currentValue)
+  }
+  
+  @IBOutlet weak var slider: UISlider!
+  @IBOutlet weak var guessValueTextField: UITextField!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    game.startNewGame()
+    updateView()
+    
+    guessValueTextField.delegate = self
+    guessValueTextField.addTarget(self, action: #selector(guessValueChanged), for: .editingChanged)
   }
 
-
+  @IBAction func showAlert() {
+    guard let (title, message) = game.evaluatePlayerSelection(currentValue) else {
+      return
+    }
+        
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    
+    let action = UIAlertAction(title: "OK", style: .default, handler: {
+      action in
+      self.game.startNewRound()
+      self.updateView()
+    })
+    
+    alert.addAction(action)
+    
+    present(alert, animated: true, completion: nil)
+  }
+  
+  func updateView() {
+    guard let targetValue = targetValue else {
+      return
+    }
+    slider.setValue(Float(targetValue) / 100, animated: true)
+    guessValueTextField.text = ""
+    currentValue = 0
+  }
+  
+  @objc private func guessValueChanged() {
+    guard let textValue = guessValueTextField.text, let numericValue = Int(textValue) else {
+      return
+    }
+    currentValue = numericValue
+  }
+  
 }
 
