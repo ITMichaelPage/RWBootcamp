b697aa8119e37c2646782d235d5a812395f73fb3
Michael Page <mobilepage@me.com>
2020-06-19 15:52:44 +0930

    Add requested app logic
    - First person1 is asked for scores on each of the compatibility items
    - Then the same is done for person2
    - An alert is displayed with a percentage compatibility score
    - App is reset to first question as person1

--- Week04/CompatibilitySlider/CompatibilitySlider/ViewController.swift
+++ Week04/CompatibilitySlider/CompatibilitySlider/ViewController.swift
@@ -23,13 +23,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        currentPerson = person1
+        updateQuestion()
     }
 
     @IBAction func sliderValueChanged(_ sender: UISlider) {
-        print(sender.value)
     }
 
     @IBAction func didPressNextItemButton(_ sender: Any) {
+        let currentItem = compatibilityItems[currentItemIndex]
+        currentPerson?.items.updateValue(slider.value, forKey: currentItem)
+
+        if currentPerson?.items.count ?? 0 < compatibilityItems.count {
+            // Move on to next question
+            currentItemIndex += 1
+            updateQuestion()
+        } else if currentPerson == person1 {
+            // Time to switch to person2
+            currentPerson = person2
+            currentItemIndex = 0
+            updateQuestion()
+        } else {
+            // No more questions
+            let score = calculateCompatibility()
+            showCompatibilityScoreAlert(score: score)
+        }
+    }
+
+    func updateQuestion() {
+        questionLabel.text = "User \(currentPerson!.id), what do you think about..."
+        compatibilityItemLabel.text = compatibilityItems[currentItemIndex]
+        slider.setValue(2.5, animated: true)
     }
 
     func calculateCompatibility() -> String {
@@ -46,6 +70,23 @@ class ViewController: UIViewController {
         let mismatchPercentage = sumOfAllPercentages / Double(compatibilityItems.count)
         let matchString = 100 - (mismatchPercentage * 100).rounded()
         return "\(matchString)%"
+    }
+
+    func showCompatibilityScoreAlert(score: String) {
+        let title = "Results"
+        let message = "You two are \(score) compatible"
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+        let action = UIAlertAction(title: "OK", style: .default, handler: { action in
+            self.currentPerson = self.person1
+            self.currentItemIndex = 0
+            self.person1.items.removeAll()
+            self.person2.items.removeAll()
+            self.updateQuestion()
+        })
+        alert.addAction(action)
+
+        present(alert, animated: true, completion: nil)
     }
 
 }
