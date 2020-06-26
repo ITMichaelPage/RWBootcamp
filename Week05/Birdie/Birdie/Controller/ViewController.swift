8fa359ad1e992904b0e6892109423516d90a83c3
Michael Page <mobilepage@me.com>
2020-06-26 14:03:44 +0930

    Populate the table view with posts

--- Week05/Birdie/Birdie/Controller/ViewController.swift
+++ Week05/Birdie/Birdie/Controller/ViewController.swift
@@ -10,15 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var mediaPosts: [MediaPost] {
+        return MediaPostsHandler.shared.mediaPosts
+    }
+
     @IBOutlet weak var tableview: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        MediaPostsHandler.shared.getPosts()
         setUpTableView()
     }
 
     func setUpTableView() {
         // Set delegates, register custom cells, set up datasource, etc.
+        registerTableViewCells(tableView: tableview, cellIdentifiers: [.textPostCell, .imagePostCell])
     }
 
     @IBAction func didPressCreateTextPostButton(_ sender: Any) {
@@ -31,5 +37,34 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mediaPosts.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let textPost = mediaPosts[indexPath.row] as? TextPost {
+            if let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCellIdentifier.textPostCell.rawValue, for: indexPath) as? TextPostCell {
+                cell.configure(for: textPost)
+                return cell
+            }
+        } else if let imagePost = mediaPosts[indexPath.row] as? ImagePost {
+            if let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCellIdentifier.imagePostCell.rawValue, for: indexPath) as? ImagePostCell {
+                cell.configure(for: imagePost)
+                return cell
+            }
+        }
+        // Fallback value
+        return UITableViewCell()
+    }
+
+}
+
+extension ViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
