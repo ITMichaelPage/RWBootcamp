e93b48619cf5338806a9237734b506a074806a23
Michael Page <mobilepage@me.com>
2020-07-06 17:51:03 +0930

    Add Pokemon type specific color to compact cell
    Display a Pokemon's type slot 1 color as the name background color in compact Pokemon cells

--- Week06/Pokedex/Pokedex/Cells/CompactPokemonCell.swift
+++ Week06/Pokedex/Pokedex/Cells/CompactPokemonCell.swift
@@ -5,6 +5,7 @@ class CompactPokemonCell: UICollectionViewCell {
   static let reuseIdentifier = String(describing: CompactPokemonCell.self)
   
   @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var nameBackgroundView: UIView!
   @IBOutlet weak var imageView: UIImageView!
   
   override func awakeFromNib() {
@@ -16,6 +17,7 @@ class CompactPokemonCell: UICollectionViewCell {
   func configure(for pokemon: Pokemon) {
     nameLabel.text = pokemon.name
     imageView.image = UIImage(named: String(pokemon.id))
+    nameBackgroundView.layer.backgroundColor = pokemon.typeSlot1IdentifierColor?.cgColor ?? #colorLiteral(red: 1, green: 0.4374197721, blue: 0.4048473537, alpha: 1).cgColor
   }
   
 }
