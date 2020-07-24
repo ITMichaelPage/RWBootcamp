302e9ef38667a6595eb5401960f15992bb2cd8f5
Michael Page <mobilepage@me.com>
2020-07-24 22:17:14 +0930

    Add an answer cell and register it in the view controller

--- Week09/jQuiz/jQuiz/ViewController.swift
+++ Week09/jQuiz/jQuiz/ViewController.swift
@@ -9,36 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var logoImageView: UIImageView!
     @IBOutlet weak var soundButton: UIButton!
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var clueLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var scoreLabel: UILabel!
-
+    
     var clues: [Clue] = []
     var correctAnswerClue: Clue?
     var points: Int = 0
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.separatorStyle = .none
+        configureTableView()
         
         self.scoreLabel.text = "\(self.points)"
-
+        
         if SoundManager.shared.isSoundEnabled == false {
             soundButton.setImage(UIImage(systemName: "speaker.slash"), for: .normal)
         } else {
             soundButton.setImage(UIImage(systemName: "speaker"), for: .normal)
         }
-
+        
         SoundManager.shared.playSound()
         
     }
-
+    
     @IBAction func didPressVolumeButton(_ sender: Any) {
         SoundManager.shared.toggleSoundPreference()
         if SoundManager.shared.isSoundEnabled == false {
@@ -47,25 +45,38 @@ class ViewController: UIViewController {
             soundButton.setImage(UIImage(systemName: "speaker"), for: .normal)
         }
     }
-
+    
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        registerTableViewCells(tableView: tableView, cellIdentifiers: [.answerCell])
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return clues.count
     }
-
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        return cell
+        if let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCellIdentifier.answerCell.rawValue) as? AnswerCell {
+            cell.configure(for: clues[indexPath.row])
+            return cell
+        } else {
+            fatalError("Unable to configure cell!")
+        }
     }
-
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
+        
     }
+    
 }
 
