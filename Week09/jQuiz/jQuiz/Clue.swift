c4cbf548ff6519dfe96633dbeacbfa0f98897530
Michael Page <mobilepage@me.com>
2020-07-24 21:53:30 +0930

    Add Clue and Category structs

--- Week09/jQuiz/jQuiz/Clue.swift
+++ Week09/jQuiz/jQuiz/Clue.swift
@@ -8,10 +8,36 @@
 
 import Foundation
 
-struct Clue {
-
+struct Clue: Codable, Equatable, Hashable {
+    let id: Int
+    let answer, question: String
+    let points: Int? = 100
+    let categoryID: Int
+    let category: Category
+    
+    enum CodingKeys: String, CodingKey {
+        case id, answer, question
+        case points = "value"
+        case categoryID = "category_id"
+        case category
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(answer)
+    }
+    
+    static func == (lhs: Clue, rhs: Clue) -> Bool {
+        return lhs.answer == rhs.answer
+    }
 }
 
-struct Category {
-
+struct Category: Codable {
+    let id: Int
+    let title: String
+    let cluesCount: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id, title
+        case cluesCount = "clues_count"
+    }
 }
