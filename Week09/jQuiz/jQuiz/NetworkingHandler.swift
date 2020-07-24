544f594800e18dabfd73f0197ef2141f102259ef
Michael Page <mobilepage@me.com>
2020-07-24 21:51:51 +0930

    Add getRandomCategoryID method to Networking class
    Requests a random clue from the jService API and returns the clue's category id

--- Week09/jQuiz/jQuiz/NetworkingHandler.swift
+++ Week09/jQuiz/jQuiz/NetworkingHandler.swift
@@ -9,7 +9,33 @@
 import Foundation
 
 class Networking {
-
+    
     static let sharedInstance = Networking()
-
+    
+    let apiBaseURL = URL(string: "https://jservice.io/api")!
+    
+    func getRandomCategoryID(completion: @escaping (Int) -> Void) {
+        let randomClueURL = apiBaseURL.appendingPathComponent("random")
+        
+        let task = URLSession.shared.dataTask(with: randomClueURL) { (data, response, error) in
+            guard let dataResponse = data, error == nil else {
+                print(error?.localizedDescription ?? "Response Error")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let randomClue = try decoder.decode([Clue].self, from: dataResponse).first
+                guard let randomCategoryID = randomClue?.categoryID else {
+                    print("Error", "Response did not contain a clue!")
+                    return
+                }
+                completion(randomCategoryID)
+            } catch let parsingError {
+                print("Error", parsingError)
+            }
+        }
+        task.resume()
+    }
+    
 }
